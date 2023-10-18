31205a8f78cbef47b00b2b014e50c132951e88e3
Rusii4 <ruslan123321a@mail.ru>
2023-10-18 17:16:56 +0600

    dwaijdw

---
+++ src/HW3.java
@@ -0,0 +1,57 @@
+public class HW3 {
+    public static void main(String args[]) {
+
+        String flowerName = "Roses";
+        int flowerNumber = 101;
+        System.out.println(flowerName + ' ' + flowerNumber);
+
+        int a = 15;
+        int b = 9;
+        System.out.println(a + b);
+
+        int x = 3, y = 43, z = 20;
+        System.out.println(x + y + z);
+
+        int r = 3, u = 3, s = 3, l = 3;
+        System.out.println(r + u + s + l);
+
+        double t;
+        t = 5.5;
+        System.out.println(t);
+
+        final double PI = 3.14;
+        final double g = 9.8;
+        final String gorod = "Almaty";
+        System.out.println(PI);
+        System.out.println(g);
+        System.out.println(gorod);
+
+        byte bit = 14;
+        short shot = 30;
+        int in = 1823;
+        long lon = 2183L;
+        double dob = 2.31;
+        float flo = 4.23f;
+        boolean bool = true;
+        char cha = 'j';
+        String stri = "Zdarova";
+        System.out.println(bit);
+        System.out.println(shot);
+        System.out.println(in);
+        System.out.println(lon);
+        System.out.println(dob);
+        System.out.println(flo);
+        System.out.println(bool);
+        System.out.println(cha);
+
+        short h = 4;
+        int k = h;
+        System.out.println(k);
+        int m = 14;
+        byte n = (byte) m;
+        System.out.println(n);
+
+
+    }
+}
+
---
+++ src/wdwd.java
@@ -0,0 +1,9 @@
+public class wdwd {
+    public static void main(String args[]) {
+        int a = 8;
+        int b = 3;
+        int c = 0;
+        c = ++b + a + b++;
+        System.out.println(c);
+    }
+}
