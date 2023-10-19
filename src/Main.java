dd5bef9942cd4c17d33ccdb166b7f861a9c14f34
Rusii4 <ruslan123321a@mail.ru>
2023-10-19 18:31:21 +0600

    git push

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,21 @@
 public class Main {
     public static void main(String args[]) {
 
-        String flowerName = "Roses";
-        int flowerNumber = 101;
-        System.out.println(flowerName + ' ' + flowerNumber);
-
-        int a = 15;
-        int b = 9;
-        System.out.println(a + b);
-
-        int x = 3, y = 43, z = 20;
-        System.out.println(x + y + z);
-
-        int r = 3, u = 3, s = 3, l = 3;
-        System.out.println(r + u + s + l);
-
-        double t;
-        t = 5.5;
-        System.out.println(t);
-
-        final double PI = 3.14;
-        final double g = 9.8;
-        final String gorod = "Almaty";
-
-        byte bit = 14;
-        short shot = 30;
-        int in = 1823;
-        long lon = 2183L;
-        double dob = 2.31;
-        float flo = 4.23f;
-        boolean bool = true;
-        char cha = 'j';
-        String stri = "Zdarova";
-        System.out.println(bit);
-        System.out.println(shot);
-        System.out.println(in);
-        System.out.println(lon);
-        System.out.println(dob);
-        System.out.println(flo);
-        System.out.println(bool);
-        System.out.println(cha);
-
-        short h = 4;
-        int k = h;
+int x =375;
+int y = x % 10;
+int z = x/10;
+int k = z%10;
+int m = z/10;
+        System.out.println(y);
+        System.out.println(z);
         System.out.println(k);
-        int m = 14;
-        byte n = (byte) m;
-        System.out.println(n);
-        System.out.print(20*20 + "DarU ");
-        System.out.println("DarU" + 20*20);
+        System.out.println(m);
+        System.out.println(y+""+k+""+m);
 
-
-
-
-
-
+int l = 58831;
+while (l > 0) {
+    int f = l % 10;
+    System.out.print(f);
+    l= l / 10;}
     }
 }
