17dd0cc6a71868404da889fa5b1308be7ac39471
Rusii4 <147650151+Rusii4@users.noreply.github.com>
2023-12-06 13:22:59 +0600

    Add files via upload

---
+++ src/Triangle.java
@@ -0,0 +1,31 @@
+public class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double calculatePerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    public double calculateArea() {
+
+        double s = calculatePerimeter() / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle(3, 4, 5);
+
+
+        System.out.println("Периметр треугольника: " + triangle.calculatePerimeter());
+        System.out.println("Площадь треугольника: " + triangle.calculateArea());
+    }
+}
+
